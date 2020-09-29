aa16274c31fc6987e7cd9ea65e4a7c5bbce9a142
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2020-09-29 16:16:15 +0200

    opdracht5 af

---
+++ oefeningen/Opdracht4/Opdracht4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "informatica";
+            string guessedWord = "";
+
+            for (int aantal  = 0; aantal <=2; aantal++)
+            {
+                if (guessedWord != secretWord)
+                {
+                    Console.Write("Enter guess: ");
+                    guessedWord = Console.ReadLine();
+
+                }
+                if(guessedWord == secretWord)
+                {
+                    Console.WriteLine("You've won");
+                    break;
+                
+                }
+               
+
+            }
+            
+        }
+    }
+}
