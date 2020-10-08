eb4c1ffabf7d7c9cda6b60703a85fb239d33a7fe
Lawrence Zhu <lawrence.zhu@coderclass.nl>
2020-10-08 11:15:35 +0200

    Mobieltjes opdracht af!

---
+++ oefeningen/Opdracht6/Opdracht6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Te
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float incl;
+            float korting;
+            float verzend;
+            Tele Tele1 = new Tele("iPhone", "iPhone XS", "Apple IOS", "Steve Jobs", 600f);
+            Tele Tele2 = new Tele("Samsung", "Galaxy S7", "Ändroid","Kim-Hyun-chull", 299f);
+            Tele Tele3 = new Tele("Google Pixel", "Pixel 3", "Chrome OS", "Sundar Pichai", 399f);
+
+            Tele[] Teles = new Tele[3];
+            Teles[0] = Tele1;
+            Teles[1] = Tele2;
+            Teles[2] = Tele3;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Teles[i].Type + " " + Teles[i].CEO);
+            }
+            
+            Console.WriteLine("Hoeveel iPhone XS wil je");
+            int aantal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hoeveel Galaxy's S7 wil je?");
+            int aantal2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hoeveel pixel3's wil je?");
+            int aantal3 = Convert.ToInt32(Console.ReadLine());
+            
+            float totaalPrijsIphone = Tele1.BTW(aantal1);
+            float totaalPrijsSamsung = Tele2.BTW(aantal2);
+            float totaalPrijsPixel = Tele3.BTW(aantal3);
+            
+            incl = totaalPrijsIphone + totaalPrijsPixel + totaalPrijsSamsung;
+            verzend = incl * 0.1f;
+            korting = incl * 0.2f;
+            if (incl >= 1000f)
+            {
+                incl = incl - korting;
+                Console.WriteLine("U krijgt " + korting + " euro korting");
+            }
+            Console.WriteLine("Alles in totaal kost " + incl + " euro");
+            Console.WriteLine("Verzendkosten zijn" + verzend + " euro");
+            }
+            
+
+                   
+
+
+        }
+
+    }
+
---
+++ oefeningen/Opdracht6/Opdracht6/Tele.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Te
+{
+	public class Tele
+	{
+		const float Btw = 0.21f;
+		public float prijs = 3.95f;
+		public string Type;
+		public string Model;
+		public string OS;
+		public string CEO;
+		
+	
+		public Tele (string nType, string nModel, string nOS, string nCEO, float nPrijs)
+        {
+			Type = nType;
+			Model = nModel;
+			OS = nOS;
+			CEO = nCEO;
+			prijs = nPrijs;
+        }
+		public float BTW(int aantal )
+        {
+			return aantal * prijs + ((aantal * prijs) * Btw);
+        }
+	}
+
+}
+
